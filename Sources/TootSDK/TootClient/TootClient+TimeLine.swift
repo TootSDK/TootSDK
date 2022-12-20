22d7dd2457aafc545d1f8b596f35207a5b5289c0
Konstantin Kostov <konstantin@headbright.be>
2022-12-20 07:55:12 +0100

    feat: add endpoint to view timeline based on a list

--- Sources/TootSDK/TootClient/TootClient+TimeLine.swift
+++ Sources/TootSDK/TootClient/TootClient+TimeLine.swift
@@ -28,4 +28,24 @@ public extension TootClient {
         return PagedResult(result: decoded, info: info)
     }
     
+    /// View statuses in the given list timeline.
+    func getListTimeline(listId: String, _ pageInfo: PagedInfo? = nil, limit: Int? = nil) async throws -> PagedResult<[Status]> {
+        let req = HttpRequestBuilder {
+            $0.url = getURL(["api", "v1", "timelines", "list", listId])
+            $0.method = .get
+            $0.query = getQueryParams(pageInfo, limit: limit)
+        }
+        let (data, response) = try await fetch(req: req)
+        let decoded = try decode([Status].self, from: data)
+        var pagination: Pagination?
+        
+        if let links = response.value(forHTTPHeaderField: "Link") {
+            pagination = Pagination(links: links)
+        }
+        
+        let info = PagedInfo(maxId: pagination?.maxId, minId: pagination?.minId, sinceId: pagination?.sinceId)
+        
+        return PagedResult(result: decoded, info: info)
+    }
+    
 }
