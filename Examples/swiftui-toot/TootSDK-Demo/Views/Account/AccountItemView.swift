948a3752cbaf1b97983d86d6209358278a3978e8
Konstantin Kostov <konstantin@headbright.be>
2023-01-29 03:31:54 +0100

    chore: remove commented code

--- Examples/swiftui-toot/TootSDK-Demo/Views/Account/AccountItemView.swift
+++ Examples/swiftui-toot/TootSDK-Demo/Views/Account/AccountItemView.swift
@@ -30,31 +30,3 @@ struct AccountItemView<Content: View>: View {
         }
     }
 }
-
-//
-//struct AccountItemView<Content>: View where Content: View {
-//    var description: String
-//    var value: String?
-//
-//    let content: () -> Content?
-//
-//    init(description: String, value: String) {
-//        self.init(description: description, value: value, content: EmptyView())
-//    }
-//
-//    init(description: String,
-//         value: String? = nil,
-//content: @ViewBuilder
-//        self.description = description
-//        self.value = value
-//        self.content = content
-//    }
-//
-//    var body: some View {
-//        HStack {
-//            Text(description + ": ")
-//            Text(value ?? "")
-//            content()
-//        }
-//    }
-//}
