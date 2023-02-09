a552cf22e29b567e13ea55226095d4cd37668160
Konstantin Kostov <konstantin@headbright.be>
2023-02-09 07:32:57 +0100

    docs: renerate docs, updating copyright notice so it doesn't include a last updated date

--- Scripts/updatedocs.sh
+++ Scripts/updatedocs.sh
@@ -1,4 +1,4 @@
 # At the moment we're just running jazzy, and this assumes you're calling the script from the root as
 # ./Scripts/updatedocs
 
-jazzy --module TootSDK
+jazzy --module TootSDK --copyright "Copyright (c) 2022, Konstantin Kostov and David Gary Wood"
