21c5def64f9edb8eeb6dfb3a3bba4195a94679e0
konstantin <konstantin@headbright.be>
2023-04-23 12:40:53 +0200

    chore: formatting

--- Sources/TootSDK/HTTP/URLSession+asyncData.swift
+++ Sources/TootSDK/HTTP/URLSession+asyncData.swift
@@ -17,24 +17,24 @@ extension URLSession {
 
 extension URLSession {
     func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-       var dataTask: URLSessionDataTask?
+        var dataTask: URLSessionDataTask?
         let onCancel = { dataTask?.cancel() }
 
-       return try await withTaskCancellationHandler {
+        return try await withTaskCancellationHandler {
             try await withCheckedThrowingContinuation { continuation in
-                 dataTask = self.dataTask(with: request) { data, response, error in
-                        guard let data = data, let response = response else {
-                            let error = error ?? URLError(.badServerResponse)
-                            return continuation.resume(throwing: error)
-                        }
-                        continuation.resume(returning: (data, response))
+                dataTask = self.dataTask(with: request) { data, response, error in
+                    guard let data = data, let response = response else {
+                        let error = error ?? URLError(.badServerResponse)
+                        return continuation.resume(throwing: error)
                     }
+                    continuation.resume(returning: (data, response))
+                }
 
-                    dataTask?.resume()
+                dataTask?.resume()
             }
         } onCancel: {
             onCancel()
         }
-        
+
     }
 }
