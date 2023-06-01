f1f11cd35124c8cbdb335f53b8e1d65bda4a7f5b
Philip Chu <technicat@users.noreply.github.com>
2023-06-01 10:44:40 -0600

    include ProfileDirectoryParams in directory query
    follow same pattern as search

--- Sources/TootSDK/Models/ProfileDirectoryParams.swift
+++ Sources/TootSDK/Models/ProfileDirectoryParams.swift
@@ -1,6 +1,6 @@
 //
 //  ProfileDirectoryParams.swift
-//  
+//
 //
 //  Created by Philip Chu on 5/29/23.
 //
@@ -8,20 +8,29 @@
 import Foundation
 
 public struct ProfileDirectoryParams: Codable {
-    
+
     /// Use active to sort by most recently posted statuses (default) or new to sort by most recently created profiles.
     public var order: Order?
     /// If true, returns only local accounts.
     public var local: Bool?
 
-    public init( order: Order? = nil,
-                 local: Bool? = nil) {
+    public init(order: Order? = nil,
+                local: Bool? = nil) {
         self.order = order
         self.local = local
     }
-    
+
     public enum Order: String, Codable, Hashable, CaseIterable {
         case active
         case new
+    }
+}
+
+extension ProfileDirectoryParams {
+    var queryItems: [URLQueryItem] {
+        [
+            URLQueryItem(name: "order", value: order?.rawValue),
+            URLQueryItem(name: "local", value: local?.description)
+        ].filter { $0.value != nil }
     }
 }
--- Sources/TootSDK/TootClient/TootClient+ProfileDirectory.swift
+++ Sources/TootSDK/TootClient/TootClient+ProfileDirectory.swift
@@ -1,6 +1,6 @@
 //
 //  TootClient+Directory.swift
-//  
+//
 //
 //  Created by Philip Chu on 5/29/23.
 //
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension TootClient {
-    
+
     /// List accounts visible in the directory.
     ///
     /// - Parameters:
@@ -17,13 +17,13 @@ public extension TootClient {
     ///   - order. Use active to sort by most recently posted statuses (default) or new to sort by most recently created profiles.
     ///   - local. If true, returns only local accounts.
     /// - Returns: Array of ``Account``.
-    func getProfileDirectory(offset: Int? = nil, limit: Int? = nil, params: ProfileDirectoryParams? = nil) async throws -> [Account] {
+    func getProfileDirectory(params: ProfileDirectoryParams, offset: Int? = nil, limit: Int? = nil) async throws -> [Account] {
         let req = HTTPRequestBuilder {
             $0.url = getURL(["api", "v1", "directory"])
             $0.method = .get
-            $0.query = getQueryParams(limit: limit, offset: offset)
+            $0.query = getQueryParams(limit: limit, offset: offset) + params.queryItems
         }
-        
+
         return try await fetch([Account].self, req)
     }
 }
