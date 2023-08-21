b98f29eba1cf2d516280c180ddc1bb515e08c34d
Philip Chu <technicat@users.noreply.github.com>
2023-08-20 22:49:27 -0600

    test: Tag and FeaturedTag decode

--- Package.swift
+++ Package.swift
@@ -48,6 +48,7 @@ let package = Package(
                 .copy("Resources/account.json"),
                 .copy("Resources/account_moved.json"),
                 .copy("Resources/activity.json"),
+                .copy("Resources/featured_tag.json"),
                 .copy("Resources/list.json"),
                 .copy("Resources/post no emojis.json"),
                 .copy("Resources/post with emojis and attachments.json"),
@@ -57,7 +58,8 @@ let package = Package(
                 .copy("Resources/pleroma.json"),
                 .copy("Resources/scheduled_post_sensitive.json"),
                 .copy("Resources/scheduled_post_attachment.json"),
-                .copy("Resources/scheduled_post_reply.json")
+                .copy("Resources/scheduled_post_reply.json"),
+                .copy("Resources/tag.json")
             ]),
     ]
 )
---
+++ Tests/TootSDKTests/TagTests.swift
@@ -0,0 +1,44 @@
+//
+//  TagTests.swift
+//  
+//
+//  Created by Philip Chu on 8/20/23.
+//
+
+import XCTest
+@testable import TootSDK
+
+final class TagTests: XCTestCase {
+    
+    func testTagDecode() throws {
+        // arrange
+        let json = localContent("tag")
+        let decoder = TootDecoder()
+        
+        // act
+        let result = try decoder.decode(Tag.self, from: json)
+        
+        // assert
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result.name, "tootsdk")
+        XCTAssertEqual(result.following, false)
+        XCTAssertNotNil(result.history)
+    }
+    
+    func testFeaturedTagDecode() throws {
+        // arrange
+        let json = localContent("featured_tag")
+        let decoder = TootDecoder()
+        
+        // act
+        let result = try decoder.decode(FeaturedTag.self, from: json)
+        
+        // assert
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result.name, "tootsdk")
+        XCTAssertEqual(result.id, "384027")
+        XCTAssertNotNil(result.lastPostAt)
+    }
+    
+}
+
