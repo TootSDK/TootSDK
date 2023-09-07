674bbc82c218e9750890d10fc27dc873c1e79104
Philip Chu <technicat@users.noreply.github.com>
2023-09-07 10:37:40 -0600

    test: decode FamiliarFollowers with empty follower list

--- Package.swift
+++ Package.swift
@@ -49,6 +49,7 @@ let package = Package(
                 .copy("Resources/account_moved.json"),
                 .copy("Resources/account_pixelfed.json"),
                 .copy("Resources/activity.json"),
+                .copy("Resources/familiar_followers_nofollowers.json"),
                 .copy("Resources/featured_tag.json"),
                 .copy("Resources/featured_tag_unused.json"),
                 .copy("Resources/list.json"),
