9f1cc6135bfa8bdd690d18e63b4e8b2da27750d9
Philip Chu <technicat@users.noreply.github.com>
2023-11-19 14:50:50 -0700

    add TootClient.dismissAnnouncement

--- Sources/TootSDK/TootClient/TootClient+Announcements.swift
+++ Sources/TootSDK/TootClient/TootClient+Announcements.swift
@@ -1,14 +1,14 @@
 //
 //  TootClient+Announcements.swift
-//  
+//
 //
 //  Created by Philip Chu on 10/19/23.
 //
 
 import Foundation
 
-
 public extension TootClient {
+
     /// See all currently active announcements set by admins.
     func getAnnouncements() async throws -> [Announcement] {
         try requireFeature(.announcements)
@@ -16,7 +16,18 @@ public extension TootClient {
             $0.url = getURL(["api", "v1", "announcements"])
             $0.method = .get
         }
+
         return try await fetch([Announcement].self, req)
+    }
+
+    /// Dismiss a single notification from the server.
+    func dismissAnnouncement(id: String) async throws {
+        let req = HTTPRequestBuilder {
+            $0.url = getURL(["api", "v1", "announcements", id, "dismiss"])
+            $0.method = .post
+        }
+
+        _ = try await fetch(req: req)
     }
 }
 
@@ -26,4 +37,3 @@ extension TootFeature {
     ///
     public static let announcements = TootFeature(supportedFlavours: [.mastodon, .akkoma, .pleroma])
 }
-
