2fa1b4821a88b80e32232e7fa16f88e1380bc050
Philip Chu <technicat@users.noreply.github.com>
2023-12-20 18:21:46 -0700

    ExtendedDescription.updatedAt is optional
    not in spec but in practice, e.g. social.lightbeamapps.com

--- Sources/TootSDK/Models/ExtendedDescription.swift
+++ Sources/TootSDK/Models/ExtendedDescription.swift
@@ -10,12 +10,13 @@ import Foundation
 public struct ExtendedDescription: Codable, Hashable {
 
     /// A timestamp of when the extended description was last updated.
-    public var updatedAt: Date
+    /// Note: this is not optional in the Mastodon spec but in practice sometimes null
+    public var updatedAt: Date?
     
     /// The rendered HTML content of the extended description.
     public var content: String
 
-    public init(updatedAt: Date, content: String) {
+    public init(updatedAt: Date? = nil, content: String) {
         self.updatedAt = updatedAt
         self.content = content
     }
