c2774fc2e96a56001facdd6804253865200f2e20
Philip Chu <technicat@users.noreply.github.com>
2024-01-03 12:29:58 -0700

    don't add escaping backslashes in debug json output

--- Sources/TootSDK/HTTP/Data+prettyPrintedJSONString.swift
+++ Sources/TootSDK/HTTP/Data+prettyPrintedJSONString.swift
@@ -7,7 +7,7 @@ extension Data {
     /// NSString gives us a nice sanitized debugDescription
     var prettyPrintedJSONString: NSString? {
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
-            let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+            let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted, .withoutEscapingSlashes]),
             let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
         else { return nil }
 
