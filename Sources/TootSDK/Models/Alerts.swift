579f3f5ebccbe07db908c191aa03f235d4331753
Łukasz Rutkowski <lukus178@gmail.com>
2024-01-05 20:19:47 +0100

    Add update parameter to Alerts init

--- Sources/TootSDK/Models/Alerts.swift
+++ Sources/TootSDK/Models/Alerts.swift
@@ -4,7 +4,16 @@
 import Foundation
 
 public struct Alerts: Codable, Hashable, Sendable {
-    public init(follow: Bool, favourite: Bool, repost: Bool, mention: Bool, poll: Bool? = nil, followRequest: Bool? = nil, post: Bool? = nil) {
+    public init(
+        follow: Bool,
+        favourite: Bool,
+        repost: Bool,
+        mention: Bool,
+        poll: Bool? = nil,
+        followRequest: Bool? = nil,
+        post: Bool? = nil,
+        update: Bool? = nil
+    ) {
         self.follow = follow
         self.favourite = favourite
         self.repost = repost
@@ -12,21 +21,24 @@ public struct Alerts: Codable, Hashable, Sendable {
         self.poll = poll
         self.followRequest = followRequest
         self.post = post
+        self.update = update
     }
 
-    /// Receive a push notification when someone has followed you? Boolean.
+    /// Receive a push notification when someone has followed you.
     public var follow: Bool
-    /// Receive a push notification when a post you created has been favourited by someone else? Boolean.
+    /// Receive a push notification when a post you created has been favourited by someone else.
     public var favourite: Bool
-    /// Receive a push notification when a post you created has been boosted by someone else? Boolean.
+    /// Receive a push notification when a post you created has been boosted by someone else.
     public var repost: Bool
-    /// Receive a push notification when someone else has mentioned you in a post? Boolean.
+    /// Receive a push notification when someone else has mentioned you in a post.
     public var mention: Bool
-    /// Receive a push notification when a poll you voted in or created has ended? Boolean. Added in 2.8.0
+    /// Receive a push notification when a poll you voted in or created has ended. Added in 2.8.0
     public var poll: Bool?
+    /// Receive a push notification when someone requested to follow you.
     public var followRequest: Bool?
+    /// Receive a push notification when someone you enabled notifications for has submitted a post.
     public var post: Bool?
-
+    /// Receive a push notification when a post you boosted with has been edited.
     public var update: Bool?
 
     enum CodingKeys: String, CodingKey {
