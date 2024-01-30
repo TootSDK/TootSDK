278e86b5a0050f421de86b7846367b72008a0cdb
Philip Chu <technicat@users.noreply.github.com>
2024-01-30 07:37:40 -0700

    translation spoiler_warning is spoiler_text

--- Sources/TootSDK/Models/Translation.swift
+++ Sources/TootSDK/Models/Translation.swift
@@ -10,7 +10,8 @@ import Foundation
 public struct Translation: Codable {
     
     public var content: String
-    public var spoilerWarning: String
+ // mastodon spec incorrectly lists as spoiler_warning
+    public var spoilerText: String
     public var detectedSourceLanguage: String
     public var provider: String
 }
