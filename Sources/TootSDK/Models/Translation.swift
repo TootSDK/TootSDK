681e68cfb72e87ded8ad15e5535578dd210e56d9
Philip Chu <technicat@users.noreply.github.com>
2024-02-04 23:14:55 -0700

    added language property and property doc to Translation

--- Sources/TootSDK/Models/Translation.swift
+++ Sources/TootSDK/Models/Translation.swift
@@ -9,9 +9,17 @@ import Foundation
 
 public struct Translation: Codable {
 
+    /// HTML-encoded translated content of the status.
     public var content: String
-    // mastodon spec incorrectly lists as spoiler_warning
+    /// The translated spoiler warning of the status.
+    /// The Mastodon spec incorrectly lists this as spoiler_warning
     public var spoilerText: String
+    /// The language of the source text, as auto-detected by the machine translation provider.
+    /// ISO 639 language code
     public var detectedSourceLanguage: String
+    /// The service that provided the machine translation.
     public var provider: String
+    /// The target language
+    /// Not in the Mastodon spec, but in the Mastodon code
+    public var language: String
 }
