633383e49ff32a8e4870fc7c7adfe76e7506df71
Philip Chu <technicat@users.noreply.github.com>
2024-03-27 08:26:48 -0600

    filled out verify credentials test

--- Tests/TootSDKTests/AccountTests.swift
+++ Tests/TootSDKTests/AccountTests.swift
@@ -109,6 +109,23 @@ final class AccountTests: XCTestCase {
         // assert
         XCTAssertNotNil(result)
         XCTAssertNotNil(result.source)
+        XCTAssertEqual(result.source?.fields[0].name, "Person LLC")
+        XCTAssertEqual(result.source?.fields[0].value, "https://example.com/")
+        XCTAssertNotNil(result.source?.fields[0].verifiedAt)
+        XCTAssertEqual(result.source?.sensitive, false)
+        XCTAssertEqual(result.source?.note, "Lorem ipsum")
+        XCTAssertEqual(result.source?.indexable, true)
+        XCTAssertEqual(result.source?.privacy, .public)
+        XCTAssertEqual(result.source?.hideCollections, true)
+        XCTAssertEqual(result.source?.language, "en")
+        XCTAssertEqual(result.source?.followRequestsCount, 0)
+        XCTAssertEqual(result.source?.discoverable, true)
         XCTAssertNotNil(result.role)
+        XCTAssertNotNil(result.role)
+        XCTAssertEqual(result.role?.id, "-99")
+        XCTAssertEqual(result.role?.permissions, "65536")
+        XCTAssertEqual(result.role?.highlighted, false)
+        XCTAssertEqual(result.role?.name, "")
+        XCTAssertEqual(result.role?.color, "")
     }
 }
