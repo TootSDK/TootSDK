2000ad1037e9bb29ebdf0cb0abfafc09c5497aa6
Łukasz Rutkowski <lukus178@gmail.com>
2024-07-01 19:29:11 +0200

    API to create a filter

--- Sources/TootSDK/TootClient/TootClient+Filters.swift
+++ Sources/TootSDK/TootClient/TootClient+Filters.swift
@@ -22,8 +22,9 @@ extension TootClient {
     }
 
     /// Obtain a single filter group owned by the current user.
+    ///
     /// - Parameters:
-    ///     - id: The ID of the Filter in the database.
+    ///   - id: The ID of the Filter in the database.
     /// - Returns: the Filter, if successful, throws an error if not
     public func getFilter(id: String) async throws -> Filter {
         try requireFeature(.filtersV2)
@@ -35,8 +36,9 @@ extension TootClient {
     }
 
     /// Delete a filter
-    /// /// - Parameters:
-    ///     - id: The ID of the Filter in the database.
+    ///
+    /// - Parameters:
+    ///   - id: The ID of the Filter in the database.
     public func deleteFilter(id: String) async throws {
         try requireFeature(.filtersV2)
         let req = HTTPRequestBuilder {
@@ -45,7 +47,21 @@ extension TootClient {
         }
         _ = try await fetch(req: req)
     }
-
+    
+    /// Create a filter.
+    ///
+    /// - Parameter params: Parameters of filter to create.
+    /// - Returns: The created filter.
+    @discardableResult
+    public func createFilter(_ params: CreateFilterParams) async throws -> Filter {
+        try requireFeature(.filtersV2)
+        let req = try HTTPRequestBuilder {
+            $0.url = getURL(["api", "v2", "filters"])
+            $0.method = .post
+            $0.body = try .form(queryItems: params.queryItems)
+        }
+        return try await fetch(Filter.self, req)
+    }
 }
 
 extension TootFeature {
