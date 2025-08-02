67a91d0254ec57e403a9f42244981d4d6a1b8fdd
Dale Price <daprice@mac.com>
2025-08-02 16:10:56 -0500

    Add test and fix error when parsing Posts from Akkoma with quotes

--- Tests/TootSDKTests/QuoteTests.swift
+++ Tests/TootSDKTests/QuoteTests.swift
@@ -19,10 +19,11 @@ final class QuoteTests: XCTestCase {
         let result: Quote = try decoder.decode(Quote.self, from: json)
 
         // assert
-        XCTAssertEqual(result.state.value, Quote.QuoteState.accepted)
+        XCTAssertEqual(result.state?.value, Quote.QuoteState.accepted)
         XCTAssertNotNil(result.quotedPost)
         if case let .post(quotedPost) = result.quotedPost {
             XCTAssertEqual(quotedPost.id, "103270115826048975")
+            XCTAssertNil(quotedPost.quote)
         } else {
             XCTFail("quotedPost should be a .post")
         }
@@ -37,12 +38,35 @@ final class QuoteTests: XCTestCase {
         let result: Quote = try decoder.decode(Quote.self, from: json)
 
         // assert
-        XCTAssertEqual(result.state.value, Quote.QuoteState.accepted)
+        XCTAssertEqual(result.state?.value, Quote.QuoteState.accepted)
         XCTAssertNotNil(result.quotedPost)
         if case let .postID(quotedPostID) = result.quotedPost {
             XCTAssertEqual(quotedPostID, "103270115826048975")
         } else {
             XCTFail("quotedPost should be a .postID")
+        }
+    }
+
+    func testAkkomaQuotePost() throws {
+        // arrange
+        let json = localContent("post_akkoma_quote")
+        let decoder = TootDecoder()
+
+        // act
+        let resultPost: Post = try decoder.decode(Post.self, from: json)
+        guard let resultQuote = resultPost.quote else {
+            XCTFail("quote should not be nil")
+            return
+        }
+
+        // assert
+        XCTAssertNil(resultQuote.state)
+        XCTAssertNotNil(resultQuote.quotedPost)
+        if case let .post(quotedPost) = resultQuote.quotedPost {
+            XCTAssertEqual(quotedPost.id, "Al2KABLN3tuRoeE0A4")
+            XCTAssertNil(quotedPost.quote)
+        } else {
+            XCTFail("quotedPost should be a .post")
         }
     }
 }
