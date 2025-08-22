d7b289686e9d1ec94ed88faf78b8447b3cd7e087
Tim De Jong <gitlab@thinkpractice.nl>
2025-08-22 11:34:18 +0200

    Added an example of a TootSDK graphical client using SharingGRDB

---
+++ Examples/SharingGRDBExample/SharingGRDBExample/TootSDKExampleApp.swift
@@ -0,0 +1,27 @@
+//
+//  TootSDKExampleApp.swift
+//  TootSDKExample
+//
+//  Created by Konstantin Gerry on 19/08/2025.
+//
+
+import SwiftUI
+import Dependencies
+import SharingGRDB
+
+@main
+struct TootSDKExampleApp: App {
+    
+    init()
+    {
+        prepareDependencies {
+            $0.defaultDatabase = try! appDatabase()
+        }
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }        
+    }
+}
