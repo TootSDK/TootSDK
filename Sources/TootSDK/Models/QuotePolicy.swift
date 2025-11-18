4c0f65abab811159027386c955a6d8de4541d52e
nixzhu <zhuhongxu@gmail.com>
2025-11-18 12:34:51 +0800

    Added docs for QuotePolicy

--- Sources/TootSDK/Models/QuotePolicy.swift
+++ Sources/TootSDK/Models/QuotePolicy.swift
@@ -6,7 +6,10 @@
 //
 
 public enum QuotePolicy: String, CaseIterable, Codable, Sendable {
+    /// Anybody can quote
     case `public`
+    /// Only followers can quote
     case followers
+    /// Just me (only the author will be able to quote)
     case nobody
 }
